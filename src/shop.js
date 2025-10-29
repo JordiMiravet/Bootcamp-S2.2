ed034a1e62bac17fc32602f7a5aefa85ccac6544
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-10-29 18:58:06 +0100

    feat(products): move products to JSON and restructure fetch logic

--- src/shop.js
+++ src/shop.js
@@ -1,69 +1,19 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-const products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+// --------------------------------------------------
+// product.sjon Handler
 
+let products = [];
+async function loadProducts() {
+    try{
+        const response = await fetch("src/products.json");
+        if(!response.ok) throw new Error("Error al cargar los productos")
+        products = await response.json();
+    } catch (error){
+        console.error(error);
+    }
+}
+
+// --------------------------------------------------
 const cart = [];
 const total = 0;
 
@@ -205,43 +155,48 @@ const countProduct = () => {
 
 // ---------------------------------------------------------------------------------------------------- //
 // Eventos 
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadProducts();
 
-document.querySelectorAll(".add-to-cart").forEach(button => {
-    button.addEventListener("click", () => {
-        const productId = button.getAttribute("data-product-id");
+    document.querySelectorAll(".add-to-cart").forEach(button => {
+        button.addEventListener("click", () => {
+            const productId = button.getAttribute("data-product-id");
 
-        buy(productId);
-        calculateTotal();
-        applyPromotionsCart();
-        printCart();
-        countProduct();
+            buy(productId);
+            calculateTotal();
+            applyPromotionsCart();
+            printCart();
+            countProduct();
+        });
     });
-});
 
-document.getElementById("clean-cart").addEventListener("click", () => {
-    cleanCart();
-    printCart();
-    countProduct();
-    
-})
 
-document.getElementById("cart_list").addEventListener("click", (e) => {
-    const itemId = Number(e.target.getAttribute("data-product-id"));
 
-    if(e.target.classList.contains("remove-item")){
-        removeFromCart(itemId);
-        applyPromotionsCart();
+    document.getElementById("clean-cart").addEventListener("click", () => {
+        cleanCart();
         printCart();
         countProduct();
-    }
+        
+    })
 
-    // Exercise Extra
-    // * Ya que estaba no me costaba nada añadir un boton de add junto al de deleted y así he reafirmado la logica en mi cabeza
+    document.getElementById("cart_list").addEventListener("click", (e) => {
+        const itemId = Number(e.target.getAttribute("data-product-id"));
 
-    if(e.target.classList.contains("add-item")){
-        addFromCart(itemId);
-        applyPromotionsCart();
-        printCart();
-        countProduct();
-    }
+        if(e.target.classList.contains("remove-item")){
+            removeFromCart(itemId);
+            applyPromotionsCart();
+            printCart();
+            countProduct();
+        }
+
+        // Exercise Extra
+        // * Ya que estaba no me costaba nada añadir un boton de add junto al de deleted y así he reafirmado la logica en mi cabeza
+
+        if(e.target.classList.contains("add-item")){
+            addFromCart(itemId);
+            applyPromotionsCart();
+            printCart();
+            countProduct();
+        }
+    });
 });
